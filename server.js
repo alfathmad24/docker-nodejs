94510a856f3cec9980a436a9743f3d947e69dc69
alfathmas <muhammad.ayub412@gmail.com>
2021-05-14 21:57:21 +0700

    edited

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
+
 app.get("/", (req, res) => {
-    res.send("It's work. Dockerizing node.js");
+  res.send(`appid: ${req.get("host").replace(/\D/g, "")}`);
+});
+
+app.get("/app1", (req, res) => {
+  res.send(`appid: ${req.get("host").replace(/\D/g, "")} Page: app1`);
+});
+
+app.get("/app2", (req, res) => {
+  res.send(`appid: ${req.get("host").replace(/\D/g, "")} Page: app2`);
+});
+
+app.get("/admin", (req, res) => {
+  res.send(`appid: ${req.get("host").replace(/\D/g, "")} Page: ADMIN`);
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on PORT ${PORT}`);
